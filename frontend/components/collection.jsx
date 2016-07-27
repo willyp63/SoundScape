49e6d56d5a046951a548c91392af423a09c012cb
Wil Pirino <wpirino1@gmail.com>
2016-07-27 10:51:16 -0700

    swapped collection titles

--- frontend/components/collection.jsx
+++ frontend/components/collection.jsx
@@ -4,7 +4,7 @@ const TrackIndex = require('./tracks/track_index');
 
 module.exports = React.createClass({
   getInitialState () {
-    return {indexType: "MY_TRACKS"};
+    return {indexType: "MY_LIKES"};
   },
   _fetchInitialTracks () {
     if (this.state.indexType === "MY_TRACKS") {
@@ -24,13 +24,13 @@ module.exports = React.createClass({
     return (
       <div>
         <ul className="my-navbar">
-          <li id="MY_TRACKS"
-              className={this.state.indexType === "MY_TRACKS" ? "selected" : ""}
-              onClick={this._navItemClick}>My Uploads
-          </li>
           <li id="MY_LIKES"
               className={this.state.indexType === "MY_LIKES" ? "selected" : ""}
               onClick={this._navItemClick}>My Likes
+          </li>
+          <li id="MY_TRACKS"
+              className={this.state.indexType === "MY_TRACKS" ? "selected" : ""}
+              onClick={this._navItemClick}>My Uploads
           </li>
         </ul>
         <TrackIndex fetchInitialTracks={this._fetchInitialTracks}
